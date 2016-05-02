b7574947bdcbea7add0bc297bacf1d82f1da0c9e
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-05-02 11:28:19 -0700

    add auth to user routes

---
+++ app/users/user.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const angular = require('angular');
+require('angular-route');
+
+//are we calling this parks app?
+const app = angular.module('ParksApp', ['ngRoute']);
+
+require('./../services/auth_service')(app);
+require('./../services/error_service')(app);
+
+app.controller('UserController', ['$http', '$location', 'AuthService', 'ErrorService',
+function($http, $location, AuthService, ErrorService) {
+  var mainRoute = 'http://localhost:3000/users';
+  var vm = this;
+
+
+// Auth Routes
+  vm.signUp = function(user) {
+    AuthService.createUser(user, function(err) {
+      if (err) return vm.error = ErrorService('There was a problem creating a user');
+      vm.error = ErrorService(null);
+      $location.path('/home');
+    });
+  };
+
+  vm.signOut = function() {
+    AuthService.signOut(() => {
+      $location.path('/signup');
+    });
+  };
+
+  vm.signIn = function(user) {
+    AuthService.signIn(user, (err) => {
+      if (err) return vm.error = ErrorService('Problem Signing In');
+      vm.error = ErrorService(null);
+      $location.path('/home');
+    });
+  };
+
+}]);
