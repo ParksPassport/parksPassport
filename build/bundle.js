9f8421bb6d72377aabb8e1b25d533e635f0ca868
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-05-02 14:23:54 -0700

    add module.exports

--- build/bundle.js
+++ build/bundle.js
@@ -32163,94 +32163,95 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
-
+	module.exports = function(app) {
 	// const angular = require('angular');
 	// require('angular-route');
 	//
 	// const app = angular.module('app', ['ngRoute']);
 
-	__webpack_require__(6)(app);
-	__webpack_require__(7)(app);
+	  __webpack_require__(6)(app);
+	  __webpack_require__(7)(app);
 
-	app.controller('UsersController', ['$http', '$location', 'AuthService', 'ErrorService',
-	function($http, $location, AuthService, ErrorService) {
-	  var mainRoute = 'http://localhost:3000/users';
-	  var vm = this;
-	  vm.users = ['users'];
-	  vm.error = ErrorService();
+	  app.controller('UsersController', ['$http', '$location', 'AuthService', 'ErrorService',
+	  function($http, $location, AuthService, ErrorService) {
+	    var mainRoute = 'http://localhost:3000/users';
+	    var vm = this;
+	    vm.users = ['users'];
+	    vm.error = ErrorService();
 
-	  vm.getUsers = function() {
-	    $http.get(mainRoute, {
-	      headers: {
-	        token: AuthService.getToken()
+	    vm.getUsers = function() {
+	      $http.get(mainRoute, {
+	        headers: {
+	          token: AuthService.getToken()
+	        }
+	      })
+	      .then(function (result) {
+	        vm.error = ErrorService(null);
+	        vm.people = result.data;
+	      }, (err) => {
+	        vm.error = ErrorService('Please Sign In');
+	        console.log(err);
+	        $location.path('/signup');
+	      });
+	    };
+
+	    vm.createUser = function(user) {
+	      $http.post(mainRoute, user, {
+	        headers: {
+	          token: AuthService.getToken()
+	        }
+	      })
+	      .then(function(res) {
+	        vm.users = vm.users.filter((u) => u._id != user._id);
+	      });
+	    };
+
+	    vm.updateUser = function(user) {
+	      $http.put(mainRoute + '/' + user._id, user, {
+	        headers: {
+	          token: AuthService.getToken()
+	        }
+	      })
+	      .then((res) => {
+	        user.editing = false;
+	      }, (err) => console.log(err));
+	    };
+
+	    vm.toggleForm = function(user) {
+	      if (!user.editing) {
+	        user.backupName = user.name;
+	        user.editing = true;
+	      } else {
+	        user.name = user.backupName;
+	        user.editing = false;
 	      }
-	    })
-	    .then(function (result) {
-	      vm.error = ErrorService(null);
-	      vm.people = result.data;
-	    }, (err) => {
-	      vm.error = ErrorService('Please Sign In');
-	      console.log(err);
-	      $location.path('/signup');
-	    });
-	  };
+	    };
 
-	  vm.createUser = function(user) {
-	    $http.post(mainRoute, user, {
-	      headers: {
-	        token: AuthService.getToken()
-	      }
-	    })
-	    .then(function(res) {
-	      vm.users = vm.users.filter((u) => u._id != user._id);
-	    });
-	  };
+	  // Auth Routes
+	    vm.signUp = function(user) {
+	      AuthService.createUser(user, function(err) {
+	        if (err) return vm.error = ErrorService('There was a problem creating a user');
+	        vm.error = ErrorService(null);
+	        $location.path('/home');
+	      });
+	    };
 
-	  vm.updateUser = function(user) {
-	    $http.put(mainRoute + '/' + user._id, user, {
-	      headers: {
-	        token: AuthService.getToken()
-	      }
-	    })
-	    .then((res) => {
-	      user.editing = false;
-	    }, (err) => console.log(err));
-	  };
+	    vm.signOut = function() {
+	      AuthService.signOut(() => {
+	        $location.path('/signup');
+	      });
+	    };
 
-	  vm.toggleForm = function(user) {
-	    if (!user.editing) {
-	      user.backupName = person.name;
-	      user.editing = true;
-	    } else {
-	      user.name = user.backupName;
-	      user.editing = false;
-	    }
-	  };
+	    vm.signIn = function(user) {
+	      AuthService.signIn(user, (err) => {
+	        if (err) return vm.error = ErrorService('Problem Signing In');
+	        vm.error = ErrorService(null);
+	        $location.path('/home');
+	      });
+	    };
 
-	// Auth Routes
-	  vm.signUp = function(user) {
-	    AuthService.createUser(user, function(err) {
-	      if (err) return vm.error = ErrorService('There was a problem creating a user');
-	      vm.error = ErrorService(null);
-	      $location.path('/home');
-	    });
-	  };
-
-	  vm.signOut = function() {
-	    AuthService.signOut(() => {
-	      $location.path('/signup');
-	    });
-	  };
-
-	  vm.signIn = function(user) {
-	    AuthService.signIn(user, (err) => {
-	      if (err) return vm.error = ErrorService('Problem Signing In');
-	      vm.error = ErrorService(null);
-	      $location.path('/home');
-	    });
-	  };
-
-	}]);
+	  }]);
+	};
 
 
 /***/ }
