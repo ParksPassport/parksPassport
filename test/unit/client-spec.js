322365dedf473ca68d2efd45bc6e47a7cc26c7ee
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-05-06 09:48:58 -0700

    add working tests

--- test/unit/client-spec.js
+++ test/unit/client-spec.js
@@ -5,41 +5,53 @@ const angular = require('angular');
 require('angular-mocks');
 
 describe('it should test something', () => {
-  var UsersController;
+  var ParksController;
   it('should have a test', ()=> {
     expect(false).toBe(false);
   });
 
-  // beforeEach(angular.mock.module('app'));
-  // beforeEach(angular.mock.inject(function($controller) {
-  //   UsersController = $controller('UsersController');
-  // }));
-  //
-  // it('should construct a controller', () => {
-  //   expect(typeof UsersController).tobe('object');
-  //   expect(typeof UsersController.createUser).toBe('function');
-  // });
-  //
-  // describe('REST test', () => {
-  //   var $httpBackend;
-  //
-  //   beforeEach(angular.mock.inject(function(_$httpBackend_) {
-  //     $httpBackend = _$httpBackend_;
-  //   }));
-  //
-  //   afterEach(()=> {
-  //     $httpBackend.verifyNoOutstandingExpectation();
-  //     $httpBackend.verifyNoOutstandingRequest();
-  //   });
-  //
-  //   it('should get all users', () => {
-  //     $httpBackend.expectGET('http://localhost:3000/users', {name: 'test user name'})
-  //       .respond(200, {name: 'test user name', password: 'password', _id: 'uniqueid'});
-  //     UsersController.createUser({name: 'test user name'});
+  beforeEach(angular.mock.module('app'));
+  beforeEach(angular.mock.inject(function($controller) {
+    ParksController = $controller('ParksController');
+  }));
+
+  it('should construct a controller', () => {
+    expect(typeof ParksController).toBe('object');
+    expect(typeof ParksController.createPark).toBe('function');
+  });
+
+  describe('REST test', () => {
+    var $httpBackend;
+
+    beforeEach(angular.mock.inject(function(_$httpBackend_) {
+      $httpBackend = _$httpBackend_;
+    }));
+
+    afterEach(()=> {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should get create a new park', () => {
+      $httpBackend.expectPOST('http://localhost:3000/parks', {name: 'test park name'})
+        .respond(200, {name: 'test park name', _id: 'uniqueid'});
+      ParksController.createPark({name: 'test park name'});
+      $httpBackend.flush();
+      expect(ParksController.parks.length).toBeGreaterThan(0);
+      expect(ParksController.parks[1].name).toBe('test park name')
+    });
+
+  //   it('should get all parks', () => {
+  //     $httpBackend.expectGET('http://localhost:3000/parks')
+  //     .respond(200, {data: [{name: 'test park'}]});
+  //     ParksController.getParks();
   //     $httpBackend.flush();
-  //     expect(UsersController.users.length).toBeGreaterThan(0);
-  //     expect(UsersController.users[1].name).toBe('test user name')
-  //   })
-  // })
+  //     expect(ParksController.parks.length).toBe(1);
+  //     expect(ParksController.parks[0].name).toBe('test park');
+  //   });
+  });
+
+
+
 
 });
